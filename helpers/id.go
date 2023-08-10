28b71c552252814acce186c4b82f805ee530a2ce
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-08-10 21:31:18 +0200

    Rename variable

--- helpers/id.go
+++ helpers/id.go
@@ -43,14 +43,14 @@ func GenerateRandomBytes(n int) ([]byte, error) {
 // number generator fails to function correctly, in which
 // case the caller should not continue.
 func GenerateRandomString(n int) (string, error) {
-	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+	const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
 	ret := make([]byte, n)
 	for i := 0; i < n; i++ {
-		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(alphabet))))
 		if err != nil {
 			return "", err
 		}
-		ret[i] = letters[num.Int64()]
+		ret[i] = alphabet[num.Int64()]
 	}
 
 	return string(ret), nil
