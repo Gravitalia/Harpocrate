5a47898900ed1cf7096cd2265c41b17ded9a006f
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-08-11 16:11:26 +0200

    Remove useless functions

--- helpers/id.go
+++ helpers/id.go
@@ -2,41 +2,8 @@ package helpers
 
 import (
 	"crypto/rand"
-	"fmt"
-	"io"
 	"math/big"
 )
-
-// assertAvailablePRNG checks if a cryptographically secure PRNG is available.
-// Panic otherwise.
-func assertAvailablePRNG() {
-	buf := make([]byte, 1)
-
-	_, err := io.ReadFull(rand.Reader, buf)
-	if err != nil {
-		panic(
-			fmt.Sprintf(
-				"crypto/rand is unavailable: Read() failed with %#v",
-				err,
-			),
-		)
-	}
-}
-
-// GenerateRandomBytes returns securely generated random bytes.
-// It will return an error if the system's secure random
-// number generator fails to function correctly, in which
-// case the caller should not continue.
-func GenerateRandomBytes(n int) ([]byte, error) {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
-	// Note that err == nil only if we read len(b) bytes.
-	if err != nil {
-		return nil, err
-	}
-
-	return b, nil
-}
 
 // GenerateRandomString returns a securely generated random string.
 // It will return an error if the system's secure random
