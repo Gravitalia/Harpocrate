617df60bf4f7f554a559432ccbecdd813c1bccce
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-08-11 13:26:26 +0200

    Add short link savers

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
 	"os"
+	"regexp"
 
 	"github.com/Gravitalia/Harpocrate/database"
+	"github.com/Gravitalia/Harpocrate/helpers"
+	"github.com/Gravitalia/Harpocrate/model"
 	"github.com/Gravitalia/Harpocrate/proto"
 	"google.golang.org/grpc"
 )
@@ -21,12 +25,48 @@ func (s *server) Upload(
 	ctx context.Context,
 	in *proto.ReduceRequest,
 ) (*proto.ReduceReponse, error) {
+	id, err := helpers.GenerateRandomString(8)
+	if err != nil {
+		log.Printf("Cannot create random string: %v", err)
+
+		return &proto.ReduceReponse{
+			Id: "",
+		}, nil
+	}
+
+	// Check if URL is valid
+	if !regexp.MustCompile(`(?m)^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$`).MatchString(in.Url) {
+		return &proto.ReduceReponse{
+			Id: "",
+		}, nil
+	}
+
+	// Get HTML content, and check if page exists
+	_, err = helpers.GetPageHTML(in.Url)
+	if err != nil {
+		return &proto.ReduceReponse{
+			Id: "",
+		}, nil
+	}
+
+	database.Session.Query(
+		"INSERT INTO harpocrate.url ( id, original_url, author, analytics, phishing ) VALUES (?, ?, ?, ?, ?);",
+		model.URL{
+			Id:          id,
+			OriginalUrl: in.Url,
+			Author:      "",
+			Analytics:   in.Opt.Number() == 1 || in.Opt.Number() == 3,
+			Phishing:    -1.456,
+		},
+	)
+
 	return &proto.ReduceReponse{
-		Id: "1",
+		Id: id,
 	}, nil
 }
 
 func main() {
+	fmt.Println(helpers.GenerateRandomString(8))
 	// Init database session, if session not initizalied,
 	// Exit with code 1, and print error message
 	if err := database.CreateSession(); err != nil {
